e1ceb61c7753241fb15699e3076077041da5c1c7
arihantXD <kamdearihant01@gmail.com>
2025-04-14 15:59:01 +0530

    done

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,2 +1,2 @@
-export { prisma } from "./client"; // exports instance of prisma
+export * from "./client"; // exports instance of prisma
 export * from "../generated/client"; // exports generated types from prisma
